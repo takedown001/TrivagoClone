496a186174621dffe43e02f35b06aa619d4c6b8b
Shailendra Regar <www.shailu786@gmail.com>
2023-01-03 13:53:23 +0530

    Initial commit

---
+++ app/src/main/java/com/trainig/trivago/MainActivity.java
@@ -0,0 +1,32 @@
+package com.trainig.trivago;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.getSettings().setLoadsImagesAutomatically(true);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webview. loadUrl("https://www.trivago.in/");
+        webview.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+
+}
